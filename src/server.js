65c8ff489b5a9a71813038ea31869a3ee9f1e236
Shekel Roman <romansekel37@gmail.com>
2024-07-11 13:18:32 +0300

    up2

--- src/server.js
+++ src/server.js
@@ -46,6 +46,7 @@ export const setupServer = () => {
         };
         
         res.status(200).json({
+            status: 200,
             message: `Successfully found contact with id ${contactId}!`,
             data: contact,
         });
